23375a67a12aeb224122a096c12ffd161724f1a9
Hedada <295405870@qq.com>
2020-09-24 08:27:57 +0800

    提交代码

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-    //asdsad
+    //asdsad啊啊啊啊啊啊
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
